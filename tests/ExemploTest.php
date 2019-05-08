987b0f1b3fbf6c29ac8ce28ce89997ceb3732f69
petroskilp <petroski@ead.uepg.br>
2019-05-08 16:39:52 -0300

    Create ExemploTest.php

---
+++ tests/ExemploTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class ExemploTest extends TestCase
+{
+    public function testTeste()
+    {
+        return true;
+    }
+
+}
